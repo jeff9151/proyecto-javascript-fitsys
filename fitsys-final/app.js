0504221502faada8febe6283db2356acc604a714
Jeffrey <lucario9151.17@gmail.com>
2023-02-12 07:03:28 -0600

    Optimizando semantica de términos en middleware

--- fitsys-final/app.js
+++ fitsys-final/app.js
@@ -10,7 +10,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -54,7 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/auth', loginRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
