554b897642fa624316a7bc30c20683bb34471dc4
Jeffrey <lucario9151.17@gmail.com>
2023-02-14 08:24:25 -0600

    Vistas básicas para recordatorio de clave, nuevo registro y confirmación de email

--- fitsys-final/routes/auth.js
+++ fitsys-final/routes/auth.js
@@ -6,9 +6,30 @@ var userAuth = require('./../controllers/authentication')
 /* Parsing vars */
 router.use(bodyParser.urlencoded({ extended: false }))
 
+/* GET Confirmación de correo de usuario */
+router.get('/confirmation', function (req, res) {
+  res.render('confirmation', { 'title': 'Confirmación de Correo electrónico' })
+})
+
+/* GET Registro de usuarios en línea */
+router.get('/register', function (req, res) {
+  res.render('register', { 'title': 'Registro de nuevo usuario' })
+})
+
+/* GET Recuperación de clave */
+router.get('/reset', function (req, res) {
+  res.render('reset', { 'title': 'Recuperación de contraseña' })
+})
+
 /* GET Autenticación */
 router.get('/login', function (req, res) {
   res.render('login', { 'title': 'Acceso al sistema' })
+})
+
+/* GET Logout */
+router.get('/logout', function (req, res) {
+  req.session = null
+  res.redirect('/')
 })
 
 /* POST Validación Usuario/Contraseña */
