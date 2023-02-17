4488ae028a68686071c43468a6aaf687df900e8d
Jeffrey <lucario9151.17@gmail.com>
2023-02-17 10:00:12 -0600

    Actualización de los imports de usuario

--- fitsys-final/controllers/forms.js
+++ fitsys-final/controllers/forms.js
@@ -1,18 +1,18 @@
-var user = require('./../forms/user.js')
+var user = require('./../forms/usuario.js')
 
 module.export = function (req, res) {
 
     switch (req.body.form_id) {
         case 'user.new':
-            var userNew = require('./../forms/user/new')
+            var userNew = require('../forms/usuario/new')
             return userNew();
             break;
         case 'user.edit':
-            var userEdit = require('./../forms/user/edit')
+            var userEdit = require('../forms/usuario/edit')
             res.send(userEdit());
             break;
         case 'user.del':
-            var userDel = require('./../forms/user/del')
+            var userDel = require('../forms/usuario/del')
             res.send(userDel());
         default:
             res.send("ERROR: FORM NOT VALID")
--- fitsys-final/models/usuario.js
+++ fitsys-final/models/usuario.js
@@ -1,4 +1,4 @@
-module.exports = class User {
+module.exports = class Usuario {
 
     pgConfig
     pool
