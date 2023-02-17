3e3790fe6556ea341fb5ba9d7965497a7fbfa4d7
Jeffrey <lucario9151.17@gmail.com>
2023-02-17 06:54:04 -0600

    Definición de ruta y controlador para el manejo del almacenamiento de los formularios en general en la base de datos

--- fitsys-final/routes/admin.js
+++ fitsys-final/routes/admin.js
@@ -1,18 +1,29 @@
 var fs = require('fs');
 var express = require('express')
 var router = express.Router()
+var form = require('./../controllers/forms')
 
 /* Redirección a página login si la sesión no ha sido definida */
 router.use((err, req, res, next) => {
   if (!req.session.auth) {
     //next(createError(403));
-    res.status(403).send('<h1>Accesso denegado (403)</h1><hr><p>Por favor asegurese de haber ingresado en el sistema con sus credenciales en la <a href="/auth/login">página de ingreso</a>.</h1>');
+    res.status(403).send('<h1>Accesso denegado (403)</h1><hr><p>Por favor asegurese de haber ingresado en el sistema con sus credenciales en la <a href="/auth/login">página de ingreso</a>.</p>');
   }
 });
 
-/* GET Autenticación */
+/* GET Admin Welcome */
 router.get('/', function (req, res) {
   res.render('layouts/admin', { 'req': req, 'res': res, 'title': 'Módulo Administrativo', 'contentPath': '../admin/index' })
+})
+
+/* POST Admin Form Storage/Update */
+router.post('/', function (req, res) {
+  if (req.xhr){
+    res.send(form.process(req, res))
+  }
+  else {
+    res.status(403).send('<h1>Accesso denegado (403)</h1><hr><p>Está intentando acceder a una página deshabilitada por el Sistema, por favor utilice la <a href="/">página principal</a> para obtener más información.</p>');
+  }
 })
 
 /* GET Admin Users */
