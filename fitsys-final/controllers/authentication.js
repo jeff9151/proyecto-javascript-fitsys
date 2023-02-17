9c81898936592aaf6865d387e5ec38d4cf056589
Jeffrey <lucario9151.17@gmail.com>
2023-02-17 05:30:46 -0600

    Adición de nuevos campos a la tabla usuarios y actualización del controlador de autenticación y definición de nuevas variables de sesión para utilizar en lugares clave de la aplicación

--- fitsys-final/controllers/authentication.js
+++ fitsys-final/controllers/authentication.js
@@ -1,68 +1,70 @@
 const { Pool } = require('pg')
-
 const pgConfig = require('./../config/pg-params')
 const pool = new Pool(pgConfig);
 
 module.exports = (req, res, user, pass) => {
 
-    console.log(pgConfig);
- 
-      
-    pool.on('error', (err, client) => {
-        console.error('Unexpected error on idle client', err)
-        process.exit(-1)
-      })
-    
-      pool.connect((err, client, done) => {
-    
-        if (err) {
-          res.send('ERROR: DB CONNECTION ERROR', err.stack)
-          process.exit(-1)
-        }
-    
-        const query = {
-          text: 'SELECT tipo FROM usuarios WHERE usuario = $1::text AND clave = CRYPT($2::text,clave)',
-          values: [user, pass],
-          rowMode: 'array'
-        }
-    
-        // if (err) throw err
-        client.query(query, (err, resu) => {
-    
-          if (err) {
-    
-            client.release()
-            console.log(err.stack);
-            res.send('ERROR: QUERY EXECUTION');
-            process.exit(-1)
-    
-          }
-    
-          else if (resu.rows.length > 0) {
-    
-            // Creando una nueva sesión
-            req.session.usuario = req.body.usuario
-            req.session.auth = true
-            req.session.tipo = resu.rows[0][0]
+  pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err)
+    process.exit(-1)
+  })
 
-            // Close connection
-            client.release()
-    
-            // Sending response
-            res.send('OK')    
-    
-          }
-    
-          else {
-    
-            res.send('ERROR: INVALID USERNAME OR PASSWORD')
-            process.exit(-1)
-    
-          }
-    
-        }) // END CLIENT
-    
-      }) // END POOL
-    
+  pool.connect((err, client, done) => {
+
+    if (err) {
+      res.send('ERROR: DB CONNECTION ERROR', err.stack)
+      process.exit(-1)
+    }
+
+    const query = {
+      text: "SELECT nombre, apellidos, tipo FROM usuario WHERE status = 1 AND email_usuario = $1::text AND clave = CRYPT($2::text,clave)",
+      values: [user, pass],
+      rowMode: 'array'
+    }
+
+    console.log(query)
+
+    // if (err) throw err
+    client.query(query, (err, resu) => {
+
+      if (err) {
+
+        client.release()
+        console.log(err.stack);
+        res.send('ERROR: SQL QUERY EXECUTION');
+        process.exit(-1)
+
+      }
+
+      else if (resu.rows.length > 0) {
+
+        console.log('Resultado Base de datos', resu.rows)
+
+        // Creando una nueva sesión
+        req.session.nombre = resu.rows[0][0]
+        req.session.apellidos = resu.rows[0][1]
+        req.session.tipo = resu.rows[0][2]
+        req.session.usuario = user
+        req.session.auth = true
+
+        // Close connection
+        client.release()
+
+        // Sending response
+        res.send('OK')
+
+      }
+
+      else {
+
+        res.send('ERROR: INVALID USERNAME OR PASSWORD')
+        process.exit(-1)
+
+      }
+
+    }) // END CLIENT
+
+  }) // END POOL
+
 }
 
