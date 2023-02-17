e0d969a07e82d59c9e74ec244f11d10d566cf121
Jeffrey <lucario9151.17@gmail.com>
2023-02-17 10:59:22 -0600

    Mapeo y validación de los campos en el Backendx

--- fitsys-final/forms/usuario/new.js
+++ fitsys-final/forms/usuario/new.js
@@ -1,1 +1,19 @@
-var usuarioModel = require('./../../models/usuario')
+import { Usuario } from './../../models/usuario';
+const user = new Usuario();
+
+module.exports = (data) => {
+    var formID = data.formID;
+    var info = []
+
+    // Mapa y validación de campos de formulario con base de datos
+    info['status'] = (data.status == 0 || data.status == 1? data.status : 0)
+    info['nombre'] = data.nombre
+    info['apellidos'] = data.apellidos
+    info['email_usuario'] = data.Usuario
+    info['clave'] = data.clave
+    info['tipo'] = data.tipo
+
+    // Almacenando datos en la BD
+    return user.new(info)
+
+}
